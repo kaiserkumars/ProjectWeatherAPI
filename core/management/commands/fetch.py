87f6c964f1d3d9ea1f45533b623bd8970e68cff2
kaiserkumars <96deepakkumar@gmail.com>
2019-02-27 17:21:06 +0530

    model initialized

--- core/management/commands/fetch.py
+++ core/management/commands/fetch.py
@@ -3,11 +3,22 @@ import requests
 
 
 class Command(BaseCommand):
-	help = 'Displays current time'
+	help = 'Fetches data'
+
+
+	def add_arguments(self,parser):
+		parser.add_argument(
+            '--metric', dest='metric', required=False,
+            help='The metric',)
+		parser.add_argument(
+            '--loc', dest='location', required=False,
+            help='The location',
+        )
 
 	def handle(self, *args, **kwargs):
-		
+		metric = kwargs['metric']
+		location = kwargs['location']
 		url = "https://s3.eu-west-2.amazonaws.com/interview-question-data/metoffice/Rainfall-UK.json"
 		response  = requests.get(url)
 		response = response.json()
-		print(response)
+		# print(response)
--- core/models.py
+++ core/models.py
@@ -1,4 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class WeatherData(models.Model):
+	location = models.CharField(max_length=50)
+	metric = models.CharField(max_length=50)
+	date_added = models.DateTimeField(auto_now=True)
+	value= models.FloatField(null=True,blank=True)
+	year=models.IntegerField()
+	month=models.IntegerField()
 
