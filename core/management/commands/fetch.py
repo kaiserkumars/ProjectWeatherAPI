b11a3be205c8cf74959815dd9c218f26e737dc4d
kaiserkumars <96deepakkumar@gmail.com>
2019-02-28 15:14:56 +0530

    UniqueConstraint on database

--- core/management/commands/fetch.py
+++ core/management/commands/fetch.py
@@ -1,11 +1,12 @@
 from django.core.management.base import BaseCommand
+from core.models import WeatherData
 import requests
 
 
 class Command(BaseCommand):
 	help = 'Fetches data'
 
-
+	
 	def add_arguments(self,parser):
 		parser.add_argument(
             '--metric', dest='metric', required=False,
@@ -16,9 +17,23 @@ class Command(BaseCommand):
         )
 
 	def handle(self, *args, **kwargs):
+		#TODO: 1. Take argument from user.  2. Implement Cache/ Avoid duplicate records
 		metric = kwargs['metric']
 		location = kwargs['location']
 		url = "https://s3.eu-west-2.amazonaws.com/interview-question-data/metoffice/Rainfall-UK.json"
-		response  = requests.get(url)
-		response = response.json()
+		response  = requests.get(url).json()
+		loc = "UK"
+		metric = "Rainfall"
+		bulk_insert=[]
+		for x in response:
+			data = WeatherData(location=loc, metric=metric,value=x['value'],year=x['year'],month=x['month'])	
+			bulk_insert.append(data)
+
+		print(len(bulk_insert))
+		try:
+			WeatherData.objects.bulk_create(bulk_insert)
+			print("Success.")
+		except Exception as e:
+			return str(e)
+
 		# print(response)
--- core/models.py
+++ core/models.py
@@ -1,10 +1,14 @@
 from django.db import models
-
+from datetime import date
 class WeatherData(models.Model):
 	location = models.CharField(max_length=50)
 	metric = models.CharField(max_length=50)
-	date_added = models.DateTimeField(auto_now=True)
+	date_added = models.DateField(auto_now_add=True)
 	value= models.FloatField(null=True,blank=True)
 	year=models.IntegerField()
 	month=models.IntegerField()
+	class Meta:
+		unique_together=(('location','metric','year','month'),)
+	def __str__(self):
+		return self.location
 
