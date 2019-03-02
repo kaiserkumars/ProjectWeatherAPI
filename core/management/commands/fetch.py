12e148f30e98b5ecbc53a52e64be1d456762a62d
kaiserkumars <96deepakkumar@gmail.com>
2019-03-02 17:45:42 +0530

    Added arguments input

--- core/management/commands/fetch.py
+++ core/management/commands/fetch.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand
 from core.models import WeatherData
+from itertools import product
 import requests
+
 
 
 class Command(BaseCommand):
@@ -10,30 +12,38 @@ class Command(BaseCommand):
 	def add_arguments(self,parser):
 		parser.add_argument(
             '--metric', dest='metric', required=False,
-            help='The metric',)
+            help='The metric')
 		parser.add_argument(
             '--loc', dest='location', required=False,
-            help='The location',
-        )
+            help='The location')
 
 	def handle(self, *args, **kwargs):
-		#TODO: 1. Take argument from user.  2. Implement Cache/ Avoid duplicate records
-		metric = kwargs['metric']
-		location = kwargs['location']
-		url = "https://s3.eu-west-2.amazonaws.com/interview-question-data/metoffice/Rainfall-UK.json"
-		response  = requests.get(url).json()
-		loc = "UK"
-		metric = "Rainfall"
+		mtList = ['Tmax','Tmin','Rainfall'] # Metric list
+		locList=['UK','England','Scotland','Wales'] # Location list
 		bulk_insert=[]
-		for x in response:
-			data = WeatherData(location=loc, metric=metric,value=x['value'],year=x['year'],month=x['month'])	
-			bulk_insert.append(data)
+		defUrl = "https://s3.eu-west-2.amazonaws.com/interview-question-data/metoffice/{met}-{loc}.json" # URL to fetch data
+		if kwargs['metric'] and kwargs['location'] is not None: # Checking for --metric and --location arguments 
+			response = requests.get(defUrl.format(met=kwargs['metric'],loc=kwargs['location'])).json()
+			for x in response:
+					data = WeatherData(location=kwargs['location'], metric=kwargs['metric'],value=x['value'],year=x['year'],month=x['month'])
+					bulk_insert.append(data)
+		else:
+			for m,l in product(mtList,locList):
+				bulk_insert=[]
+				response = requests.get(defUrl.format(met=m,loc=l)).json()
+				for x in response:
+					data = WeatherData(location=l, metric=m,value=x['value'],year=x['year'],month=x['month'])
+					bulk_insert.append(data)
+				try:
+					WeatherData.objects.bulk_create(bulk_insert)
+					print("Success.")
+				except Exception as e:
+					print(str(e))
 
-		print(len(bulk_insert))
-		try:
-			WeatherData.objects.bulk_create(bulk_insert)
-			print("Success.")
-		except Exception as e:
-			return str(e)
+				
+				
 
-		# print(response)
+				
+		
+		
+		
--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import date
+
 class WeatherData(models.Model):
 	location = models.CharField(max_length=50)
 	metric = models.CharField(max_length=50)
@@ -8,7 +9,7 @@ class WeatherData(models.Model):
 	year=models.IntegerField()
 	month=models.IntegerField()
 	class Meta:
-		unique_together=(('location','metric','year','month'),)
+		unique_together=(('location','metric','year','month'),) #To prevent duplicate records
 	def __str__(self):
 		return self.location
 
